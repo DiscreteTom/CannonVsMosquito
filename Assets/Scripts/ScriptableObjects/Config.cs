79262a5fdc469119392ece3563f240275679518f
DiscreteTom <discrete_tom@outlook.com>
2023-02-28 18:09:18 +0800

    add config.angleRange/cannonRotateSpeed

--- Assets/Scripts/ScriptableObjects/Config.cs
+++ Assets/Scripts/ScriptableObjects/Config.cs
@@ -6,4 +6,6 @@ public class Config : ScriptableObject {
   public string roomId;
   public int localPlayerId;
   public int timeout = 60;
+  public float angleRange = 160f;
+  public float cannonRotationSpeed = 180f;
 }
