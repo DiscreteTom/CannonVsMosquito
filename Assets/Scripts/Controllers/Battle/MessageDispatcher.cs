cdbc535d9af4808aa46478fc3fed1bed4a8c1220
DiscreteTom <discrete_tom@outlook.com>
2023-03-01 13:51:45 +0800

    add mock server if server url is empty

--- Assets/Scripts/Controllers/Battle/MessageDispatcher.cs
+++ Assets/Scripts/Controllers/Battle/MessageDispatcher.cs
@@ -5,59 +5,92 @@ public class MessageDispatcher : CBC {
   void Start() {
     var eb = this.Get<EventBus>();
     var config = this.Get<Config>();
+    var useMockServer = config.serverUrl == "";
 
-    // create or join server when the websocket is connected
-    eb.AddListener("ws.connected", () => {
-      if (config.localPlayerId == 0) {
-        // create room
+    if (!useMockServer) {
+      // create or join server when the websocket is connected
+      eb.AddListener("ws.connected", () => {
+        if (config.localPlayerId == 0) {
+          // create room
+          eb.Invoke("ws.send", JsonUtility.ToJson(new {
+            action = "create",
+            room = config.roomId,
+          }));
+        } else {
+          // join room
+          eb.Invoke("ws.send", JsonUtility.ToJson(new {
+            action = "join",
+            room = config.roomId,
+          }));
+        }
+      });
+
+      // process game events
+      eb.AddListener("local.shoot", (float x, float y, float angle) => {
         eb.Invoke("ws.send", JsonUtility.ToJson(new {
-          action = "create",
-          room = config.roomId,
+          action = "shoot",
+          origin = new {
+            x = x,
+            y = y,
+          },
+          angle = angle,
         }));
-      } else {
-        // join room
-        eb.Invoke("ws.send", JsonUtility.ToJson(new {
-          action = "join",
-          room = config.roomId,
+      });
+
+      // listen for messages from the server and invoke game events
+      eb.AddListener("ws.message", (string str) => {
+        var raw = JsonUtility.FromJson<ServerMessage>(str);
+        if (raw.type == "error") {
+          var msg = JsonUtility.FromJson<ErrorEvent>(str);
+          eb.Invoke("game.error", msg);
+          Debug.LogError(msg.type);
+        } else if (raw.type == "game start") {
+          var msg = JsonUtility.FromJson<GameStartEvent>(str);
+          eb.Invoke("game.start", msg);
+        } else if (raw.type == "player shoot") {
+          var msg = JsonUtility.FromJson<PlayerShootEvent>(str);
+          eb.Invoke("game.shoot", msg);
+        } else if (raw.type == "new target") {
+          var msg = JsonUtility.FromJson<NewTargetEvent>(str);
+          eb.Invoke("game.newTarget", msg);
+        } else if (raw.type == "game over") {
+          var msg = JsonUtility.FromJson<GameOverEvent>(str);
+          eb.Invoke("game.over", msg);
+        } else {
+          Debug.LogError("Unknown message type: " + raw.type);
+        }
+      });
+    } else {
+      print("Using mock server");
+      this.OnNextUpdate.AddListener(() => {
+        eb.Invoke("game.start", new GameStartEvent {
+          targets = new Target[] {
+            new Target {
+              x = 0,
+              y = 0,
+              id = 0,
+            },
+            new Target {
+              x = 0,
+              y = 0,
+              id = 1,
+            },
+          },
+        });
+      });
+
+      eb.AddListener("local.shoot", (float x, float y, float angle) => {
+        eb.Invoke("game.send", (new PlayerShootEvent {
+          player = config.localPlayerId,
+          hit = new int[] { 0, 1 },
+          origin = new Origin {
+            x = x,
+            y = y,
+          },
+          angle = angle,
         }));
-      }
-    });
-
-    // process game events
-    eb.AddListener("local.shoot", (float x, float y, float angle) => {
-      eb.Invoke("ws.send", JsonUtility.ToJson(new {
-        action = "shoot",
-        origin = new {
-          x = x,
-          y = y,
-        },
-        angle = angle,
-      }));
-    });
-
-    // listen for messages from the server and invoke game events
-    eb.AddListener("ws.message", (string str) => {
-      var raw = JsonUtility.FromJson<ServerMessage>(str);
-      if (raw.type == "error") {
-        var msg = JsonUtility.FromJson<ErrorEvent>(str);
-        eb.Invoke("game.error", msg);
-        Debug.LogError(msg.type);
-      } else if (raw.type == "game start") {
-        var msg = JsonUtility.FromJson<GameStartEvent>(str);
-        eb.Invoke("game.start", msg);
-      } else if (raw.type == "player shoot") {
-        var msg = JsonUtility.FromJson<PlayerShootEvent>(str);
-        eb.Invoke("game.shoot", msg);
-      } else if (raw.type == "new target") {
-        var msg = JsonUtility.FromJson<NewTargetEvent>(str);
-        eb.Invoke("game.newTarget", msg);
-      } else if (raw.type == "game over") {
-        var msg = JsonUtility.FromJson<GameOverEvent>(str);
-        eb.Invoke("game.over", msg);
-      } else {
-        Debug.LogError("Unknown message type: " + raw.type);
-      }
-    });
+      });
+    }
   }
 }
 
