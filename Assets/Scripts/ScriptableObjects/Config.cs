9ea7b1d3b0dbd9a1001e9c4b7cc960a17b4a1342
DiscreteTom <discrete_tom@outlook.com>
2023-03-01 13:36:16 +0800

    add config headers

--- Assets/Scripts/ScriptableObjects/Config.cs
+++ Assets/Scripts/ScriptableObjects/Config.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/Config")]
 public class Config : ScriptableObject {
+  [Header("Server")]
   public string serverUrl;
+
+  [Header("Game")]
   public string roomId;
   public int localPlayerId;
   public int timeout = 60;
+
+  [Header("Cannon")]
   public float angleRange = 160f;
   public float cannonRotationSpeed = 180f;
 }
