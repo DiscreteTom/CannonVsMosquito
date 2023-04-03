8d4dd61375bd046684996936a832343d58840b72
DiscreteTom <discrete_tom@outlook.com>
2023-04-03 17:16:21 +0800

    deploy

--- Assets/Scripts/Controllers/Battle/WaitingText.cs
+++
@@ -1,9 +0,0 @@
-using DT.General;
-
-public class WaitingText : CBC {
-  void Start() {
-    var eb = this.Get<EventBus>();
-
-    eb.AddListener("game.start", (GameStartEvent _) => this.gameObject.SetActive(false));
-  }
-}
--- Assets/Scripts/Controllers/Welcome/ExitButton.cs
+++
@@ -1,9 +0,0 @@
-using DT.General;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class ExitButton : CBC {
-  void Start() {
-    this.GetComponent<Button>().onClick.AddListener(PanicManager.Panic);
-  }
-}
