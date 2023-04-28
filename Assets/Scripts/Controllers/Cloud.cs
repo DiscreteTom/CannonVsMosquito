467fa1205b201366ac8075fe1c9c5ce60da3841d
DiscreteTom <discrete_tom@outlook.com>
2023-04-28 17:38:59 +0800

    chore: optimize float cloud using UniUtils

--- Assets/Scripts/Controllers/Cloud.cs
+++ Assets/Scripts/Controllers/Cloud.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class Cloud : CBC {
   void Start() {
-    // float up and down
-    this.onFixedUpdate.AddListener(() => {
-      this.transform.SetPositionY(this.transform.position.y + Mathf.Sin(Time.time * 2) * 0.005f);
-    });
-
     // from left to right, from -15 to 15
     this.transform.SetPositionX(-15);
     this.onFixedUpdate.AddListener(() => {
       if (this.transform.position.x < 15) {
-        this.transform.SetPositionX(this.transform.position.x + 0.005f);
+        this.transform.SetPositionX(this.transform.position.x + 0.01f);
       } else {
         this.transform.SetPositionX(-15);
       }
