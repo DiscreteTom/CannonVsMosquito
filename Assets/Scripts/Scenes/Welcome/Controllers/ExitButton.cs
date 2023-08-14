f43362495be350f2b1d2243553c378702c9b878b
DiscreteTom <discrete_tom@outlook.com>
2023-08-14 14:17:04 +0800

    feat: hide exit button when WebGL build

--- Assets/Scripts/Scenes/Welcome/Controllers/ExitButton.cs
+++ Assets/Scripts/Scenes/Welcome/Controllers/ExitButton.cs
@@ -1,10 +1,13 @@
 using DT.UniStart;
-
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Project.Scene.Welcome {
   public class ExitButton : CBC {
     void Start() {
+      if (Application.platform == RuntimePlatform.WebGLPlayer)
+        this.gameObject.SetActive(false);
+
       this.GetComponent<Button>().onClick.AddListener(UniStart.ExitGame);
     }
   }
